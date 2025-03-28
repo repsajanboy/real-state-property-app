87d50b0cce7ee210eb5736d3f83e5e5453662c53
repsajanboy <janboym@gmail.com>
2025-03-28 20:25:21 +0800

    onboarding screen and images

--- lib/presentation/onboarding/view/onboarding_screen.dart
+++ lib/presentation/onboarding/view/onboarding_screen.dart
@@ -1,10 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:real_state_property/routing/app_router_names.dart';
+import 'package:real_state_property/styles/app_images.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: TextStyle(fontSize: 19.0),
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          titleWidget: _buildTitleWidget(
+            "Don't Miss Another Lead",
+            Colors.black,
+          ),
+          bodyWidget: _buildBodyWidget(
+            "More Than 90,000 Real Estate Agents Served",
+            Colors.black,
+          ),
+          image: _buildImage(AppImages.appIcon),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          titleWidget: _buildTitleWidget(
+            "Create Your Listing",
+            Colors.black,
+          ),
+          bodyWidget: _buildBodyWidget(
+            "We make it easy to get the word out with one touch email and social sharing.",
+            Colors.black,
+          ),
+          image: _buildImage(AppImages.multiTasking, width: 300),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          titleWidget: _buildTitleWidget(
+            "Run Your Open House",
+            Colors.black,
+          ),
+          bodyWidget: _buildBodyWidget(
+            "After each open house, we'll send visitors a professional follow up email containing your contact information.",
+            Colors.black,
+          ),
+          image: _buildImage(AppImages.doorDrawing, width: 300),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          titleWidget: _buildTitleWidget(
+            "Convert More Leads Into Clients",
+            Colors.black,
+          ),
+          bodyWidget: _buildBodyWidget(
+            "We'll send you a list of leads that don't have an agent or have a home to sell, so you know where to focus your time.",
+            Colors.black,
+          ),
+          image: _buildImage(AppImages.yogaGirl, width: 220),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      showSkipButton: true,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      dotsFlex: 1,
+      skip: Text('Skip', style: TextStyle(color: Colors.black)),
+      next: Icon(
+        Icons.arrow_forward,
+        color: Colors.black,
+      ),
+      done: Text('Done', style: TextStyle(color: Colors.black)),
+      dotsDecorator: DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Colors.grey,
+        activeColor: Colors.blue,
+        activeSize: Size(20.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+    );
+  }
+
+  void _onIntroEnd(context) {
+    Navigator.pushNamed(context, RouteNames.login);
+  }
+
+  Widget _buildImage(String asset, {double width = 150.0}) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Image.asset(
+        asset,
+        width: width,
+      ),
+    );
+  }
+
+  Widget _buildTitleWidget(String text, Color color) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 24.0,
+        color: color,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _buildBodyWidget(String text, Color color) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 16.0, color: color),
+    );
   }
 }
---
+++ lib/styles/app_images.dart
@@ -0,0 +1,16 @@
+const appImagesPath = "assets/images/";
+
+class AppImages {
+  static const appIcon = '${appImagesPath}ohp_icon.png';
+  static const drawerHeaderBG = '${appImagesPath}header2.jpg';
+  static const circleAvatar = '${appImagesPath}circle_avatar.jpg';
+  static const multiTasking = '${appImagesPath}multi_tasking.png';
+  static const doorDrawing = '${appImagesPath}door_drawing.png';
+  static const yogaGirl = '${appImagesPath}yoga_girl.png';
+  static const loginBG = '${appImagesPath}backgroup_login.png';
+  static const fbIcon = '${appImagesPath}facebook_logo.png';
+  static const placesterLogo = '${appImagesPath}facebook_logo.png';
+  static const googleIcon = '${appImagesPath}google_logo.png';
+  static const sortIcon = '${appImagesPath}sort.png';
+  static const leadIcon = '${appImagesPath}lead.png';
+}
