2d526d7451cdb49442e16db3267a82fc15d4212f
repsajanboy <janboym@gmail.com>
2025-04-11 01:01:49 +0800

    Leads json, model, lead screen

---
+++ lib/data/model/leads/leads_model.dart
@@ -0,0 +1,56 @@
+class LeadsModel {
+  List<Lead>? leads;
+  LeadsModel({this.leads});
+
+  factory LeadsModel.fromJson(Map<String, dynamic> json) => LeadsModel(
+        leads: List<Lead>.from(json["leads"].map((x) => Lead.fromJson(x))),
+      );
+}
+
+class Lead {
+  int? id;
+  String? name;
+  String? email;
+  String? phone;
+  String? propertyType;
+  int? budget;
+  String? location;
+  String? preferredContactMethod;
+  String? status;
+  String? moveInDate;
+  String? financingStatus;
+  String? agentAssigned;
+  String? typeOfLead;
+
+  Lead({
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+    this.propertyType,
+    this.budget,
+    this.location,
+    this.preferredContactMethod,
+    this.status,
+    this.moveInDate,
+    this.financingStatus,
+    this.agentAssigned,
+    this.typeOfLead,
+  });
+
+  factory Lead.fromJson(Map<String, dynamic> json) => Lead(
+        id: json["id"] as int,
+        name: json["name"] as String,
+        email: json["email"] as String,
+        phone: json["phone"] as String,
+        propertyType: json["property_type"] as String,
+        budget: json["budget"] as int,
+        location: json["location"] as String,
+        preferredContactMethod: ["preferred_contact_method"] as String,
+        status: json["status"] as String,
+        moveInDate: json["move_in_date"] as String,
+        financingStatus: json["financing_status"] as String,
+        agentAssigned: ["agent_assigned"] as String,
+        typeOfLead: json["type_of_lead"] as String,
+      );
+}
---
+++ lib/presentation/my_leads/view/my_lead_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyLeadScreen extends StatelessWidget {
+  const MyLeadScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
