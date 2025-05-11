a408e8b402868353ee45c41b660a071199aa745d
repsajanboy <janboym@gmail.com>
2025-05-12 00:33:17 +0800

    Colors and textstyle class, onboarding screen

---
+++ lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  //main color
+  static const mainColor = Color(0xff023563);
+  static const mainBgColor = Color(0xffE2E9E4);
+}
---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle heading = TextStyle(
+    fontFamily: 'Anton',
+  );
+
+  static const TextStyle onboardingTitleText = TextStyle(
+    fontFamily: 'Open Sans',
+    fontSize: 24.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle onboardingBodyText = TextStyle(
+    fontFamily: 'Open Sans',
+    fontSize: 16.0,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontFamily: 'Open Sans',
+    color: Colors.black,
+  );
+}
